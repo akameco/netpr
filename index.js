8b8ae6e3da8de68cf76ac4def84cb92c69524db5
akameco <akameco.t@gmail.com>
2016-01-06 18:46:24 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var request = new XMLHttpRequest();
+var url = 'tell_your_world.mp3' 
+request.open('GET', url, true);
+request.responseType = 'arraybuffer';
+request.send();
+
+var context = new AudioContext();
+var source = context.createBufferSource();
+var buffer = null;
+request.onload = function () {
+  var res = request.response;
+  context.decodeAudioData(res, function (buf) {
+    source.buffer = buf;
+  });
+};
+
+source.connect(context.destination);
+// 再生
+source.start(0);
